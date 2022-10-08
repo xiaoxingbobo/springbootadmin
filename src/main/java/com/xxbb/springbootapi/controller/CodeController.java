d72b11c5bd3cec61f817ea981c45a73d97f495c0
小星博博 <319653018@qq.com>
2022-10-08 15:34:11 +0800

    添加代码生成

--- src/main/java/com/xxbb/springbootapi/controller/CodeController.java
+++ src/main/java/com/xxbb/springbootapi/controller/CodeController.java
@@ -1,12 +1,17 @@
 package com.xxbb.springbootapi.controller;
 
 import com.xxbb.springbootapi.service.impl.CodeService;
+import com.xxbb.springbootapi.util.ClassUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("code")
 @Api(tags = "代码生成")
@@ -25,5 +30,14 @@ public class CodeController {
     @ApiOperation(value = "单个实体生成")
     public Boolean Generate(String entityName,Boolean isCover){
         return codeService.Generate(entityName,isCover);
+    }
+    @GetMapping("classes")
+    @ApiOperation(value = "获取所有实体类")
+    public List<String> getClasses() {
+        List<String> classes=new ArrayList<>();
+        for (Class<?> item : ClassUtil.getClasses("com.xxbb.springbootapi.entity")) {
+            classes.add(item.getName().substring(item.getName().lastIndexOf('.')+1));
+        }
+        return classes;
     }
 }
