55fc8599d25b87b06d3e388437ef1518af1c2349
小星博博 <74338666+xiaoxingbobo@users.noreply.github.com>
2022-11-05 22:53:10 +0800

    Delete target directory

--- target/classes/sql/data.sql
+++
@@ -1,9 +0,0 @@
-insert into authority (create_time, is_deleted, `name`, update_time, value)
-values (NOW(), 0, '所有权限', NOW(), 'sys:all:all');
-insert into role(create_time, update_time, is_deleted, `name`)
-values (NOW(), NOW(), 0, '超级管理员');
-insert into role_authority(create_time, update_time, is_deleted, role_id, authority_id)
-values (NOW(), NOW(), 0, 1, 1);
-insert into user (create_time, update_time, is_deleted, `name`, age, sex, role_id, username, password)
-values (NOW(), NOW(), 0, '超级管理员', 18, 1, 1, 'admin',
-        '$2a$10$yndC6fntPcMlU5zcB2pczeUS8H4q4xpws2slXcfShVXNC65R20U3y');
