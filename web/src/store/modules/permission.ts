7b26b09a48eb1f22e5674b3c2ec412dc7d2037af
星月 <1553307190@qq.com>
2022-11-07 22:46:40 +0800

    注释了原来的权限路由管理

--- web/src/store/modules/permission.ts
+++ web/src/store/modules/permission.ts
@@ -37,22 +37,44 @@ export const usePermissionStore = defineStore({
     }
   },
   actions: {
-    generateRoutes(
-      type: 'admin' | 'test' | 'none',
-      routers?: AppCustomRouteRecordRaw[] | string[]
-    ): Promise<unknown> {
+    // generateRoutes(
+    //   type: 'admin' | 'test' | 'none',
+    //   routers?: AppCustomRouteRecordRaw[] | string[]
+    // ): Promise<unknown> {
+    //   return new Promise<void>((resolve) => {
+    //     let routerMap: AppRouteRecordRaw[] = []
+    //     if (type === 'admin') {
+    //       // 模拟后端过滤菜单
+    //       routerMap = generateRoutesFn2(routers as AppCustomRouteRecordRaw[])
+    //     } else if (type === 'test') {
+    //       // 模拟前端过滤菜单
+    //       routerMap = generateRoutesFn1(cloneDeep(asyncRouterMap), routers as string[])
+    //     } else {
+    //       // 直接读取静态路由表
+    //       routerMap = cloneDeep(asyncRouterMap)
+    //     }
+    //     // 动态路由，404一定要放到最后面
+    //     this.addRouters = routerMap.concat([
+    //       {
+    //         path: '/:path(.*)*',
+    //         redirect: '/404',
+    //         name: '404Page',
+    //         meta: {
+    //           hidden: true,
+    //           breadcrumb: false
+    //         }
+    //       }
+    //     ])
+    //     // 渲染菜单的所有路由
+    //     this.routers = cloneDeep(constantRouterMap).concat(routerMap)
+    //     resolve()
+    //   })
+    // },
+    generateRoutes(routers?: AppCustomRouteRecordRaw[] | string[]): Promise<unknown> {
       return new Promise<void>((resolve) => {
         let routerMap: AppRouteRecordRaw[] = []
-        if (type === 'admin') {
-          // 模拟后端过滤菜单
-          routerMap = generateRoutesFn2(routers as AppCustomRouteRecordRaw[])
-        } else if (type === 'test') {
-          // 模拟前端过滤菜单
-          routerMap = generateRoutesFn1(cloneDeep(asyncRouterMap), routers as string[])
-        } else {
-          // 直接读取静态路由表
-          routerMap = cloneDeep(asyncRouterMap)
-        }
+        // 直接读取静态路由表
+        routerMap = cloneDeep(asyncRouterMap)
         // 动态路由，404一定要放到最后面
         this.addRouters = routerMap.concat([
           {
